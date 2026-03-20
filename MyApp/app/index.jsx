4ec2caa78b5bd7d66294912d5d712ad35a81c1aa
Tehman700 <tehmanfast@gmail.com>
2026-03-20 14:02:00 +0500

    Adding spaces to check the branch working

--- MyApp/app/index.jsx
+++ MyApp/app/index.jsx
@@ -11,6 +11,9 @@ const ZOOM_LEVELS = [
   { label: '2x',  value: 0.5 },
 ]
 
+// Just checking if branch is working or not and "PS C:\Users\tehma\Desktop\React-Native-Setup-App> git checkout -b feature/addingspacesinindexjs
+// Switched to a new branch 'feature/addingspacesinindexjs"
+
 export default function Home() {
   const [permission, requestPermission] = useCameraPermissions()
   const [capturedUri, setCapturedUri]   = useState(null)
